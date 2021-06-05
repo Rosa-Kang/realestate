9eb708a8040bb737f352a6f7d355af2b9d43cbc6
Rosa Kang <therosessom@gmail.com>
2021-06-05 09:57:18 -0700

    update hero

---
+++ src/data/SliderData.js
@@ -0,0 +1,75 @@
+import ImageOne from '../images/house-1.jpg';
+import ImageTwo from '../images/house-2.jpg';
+import ImageThree from '../images/house-3.jpg';
+import ImageFour from '../images/house-4.jpg';
+
+
+export const SliderData = [
+    {    
+        title: 'Luxury Vila in Bali, Indonesia',
+        price: '$4,280,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageOne,
+        alt: 'House'
+    },
+    {    
+    title: 'House in Burnaby',
+    price: '$2,080,000',
+    path: '/homes',
+    label: 'View Home',
+    image: ImageTwo,
+    alt: 'House'
+    },
+    {    
+    title: 'House in Coquitlam',
+    price: '$8,420,000',
+    path: '/homes',
+    label: 'View Home',
+    image: ImageThree,
+    alt: 'House'
+    },
+    {    
+    title: 'House in Burquitlam',
+    price: '$6,660,000',
+    path: '/homes',
+    label: 'View Home',
+    image: ImageFour,
+    alt: 'House'
+    }
+]
+
+// export const SliderDataTwo = [
+//     {    
+//         title: 'Luxury Vila in Bali, Indonesia',
+//         price: '$4,280,000',
+//         path: '/homes',
+//         label: 'View Home',
+//         image: ImageOne,
+//         alt: 'House'
+//     },
+//     {    
+//     title: 'House in Burnaby',
+//     price: '$2,080,000',
+//     path: '/homes',
+//     label: 'View Home',
+//     image: ImageTwo,
+//     alt: 'House'
+//     },
+//     {    
+//     title: 'House in Coquitlam',
+//     price: '$8,420,000',
+//     path: '/homes',
+//     label: 'View Home',
+//     image: ImageThree,
+//     alt: 'House'
+//     },
+//     {    
+//     title: 'House in Burquitlam',
+//     price: '$6,660,000',
+//     path: '/homes',
+//     label: 'View Home',
+//     image: ImageFour,
+//     alt: 'House'
+//     }
+// ]
