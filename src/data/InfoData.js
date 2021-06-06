9469d05fb0200c04ef64c83399cb021e1ad6bb8e
Rosa Kang <therosessom@gmail.com>
2021-06-05 21:00:34 -0700

    update Infosection

---
+++ src/data/InfoData.js
@@ -0,0 +1,34 @@
+import ImageOne from "../images/interior-1.jpg";
+import ImageTwo from "../images/house-5.jpg";
+
+export const InfoData = {
+  heading: "Explore our beautiful homes",
+  paragraphOne: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+  unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, 
+  remaining essentially unchanged.`,
+  paragraphTwo: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+  unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, 
+  remaining essentially unchanged.`,
+  buttonLabel: "View Homes",
+  Image: ImageOne,
+  reverse: false,
+  delay: 100,
+};
+
+export const InfoDataTwo = {
+  heading: "Modern Designs",
+  paragraphOne: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+  unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, 
+  remaining essentially unchanged.`,
+  paragraphTwo: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+  unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, 
+  remaining essentially unchanged.`,
+  buttonLabel: "View Homes",
+  Image: ImageTwo,
+  reverse: true,
+  delay: 300,
+};
